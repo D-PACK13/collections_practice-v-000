15c98d6ea6018b664d5629e863c5ec8ca253e731
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-16 11:59:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 
+def sort_array_asc(num)
+  num.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(num)
+  num.sort.reverse do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_char_count(num)
+  num.sort_by {|x| x.length}
+end
+
+def swap_elements(num)
+  num[1], num[2] = num[2], num[1]
+  num
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(array)
+  array.each { |x| x.insert(2, "$") && x[3] = "" }
+end
+
+def find_a(array)
+  array.select { |x| x.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+    
+def add_s(array)
+  array.each { |x| x.pluralize(2) }
+end
+  
+  
